3e7cb81ad1cf78fb8844b61ee2ae62bca1f851ca
shijin p <shijincht65@gmail.com>
2024-06-29 17:05:33 +0530

    header ,hero, footer components created

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
-
+import React from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaYoutube,
+  FaPhoneAlt,
+} from "react-icons/fa";
+import logo from "../assets/logo.jpg";
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className=" py-8 px-8 sm:px-16 mt-auto">
+      {/* LOGO */}
+      <div className="w-full md:w-auto mb-20 hidden md:block ">
+        <img src={logo} alt="" />
+      </div>
+
+      <div className="container mx-auto  flex  gap-3 flex-wrap-reverse justify-between ">
+        <div className="  w-full md:w-auto mb-4 md:mb-0">
+          <h3 className="text-sm font-Poppins font-bold text-black mb-5">
+            CONNECTED WITH US
+          </h3>
+          <div className="flex space-x-4 mt-2 ">
+            <a
+              href="https://www.facebook.com"
+              className="  bg-slate-200 p-2 rounded-full text-black mr-2 hover:bg-slate-300"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://www.twitter.com"
+              className="  bg-slate-200 p-2 rounded-full text-black mr-2 hover:bg-slate-300"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://www.linkedin.com"
+              className="  bg-slate-200 p-2 rounded-full text-black mr-2 hover:bg-slate-300"
+            >
+              <FaLinkedinIn />
+            </a>
+            <a
+              href="https://www.youtube.com"
+              className="  bg-slate-200 p-2 rounded-full text-black mr-2 hover:bg-slate-300"
+            >
+              <FaYoutube />
+            </a>
+          </div>
+        </div>
+        <div className="w-full   md:w-auto mb-4 md:mb-0">
+          <h3 className="text-sm font-Poppins font-bold text-black mb-5">
+            IMPORTANT LINKS
+          </h3>
+          <div className="flex flex-col md:flex-row gap-4 text-sm">
+            <a href="#" className="text-gray-600 hover:text-gray-800">
+              Terms & Conditions
+            </a>
+            <a href="#" className="text-gray-600 hover:text-gray-800">
+              Privacy & Policy
+            </a>
+            <a href="#" className="text-gray-600 hover:text-gray-800">
+              Help & FAQs
+            </a>
+          </div>
+        </div>
+
+        <div className="w-full   md:w-auto mb-4 md:mb-0 ">
+          <div className="flex items-start flex-col gap-2  justify-center ">
+            <div className="bg-sky-500 p-2 rounded-full text-white  ">
+              <FaPhoneAlt size={20} />
+            </div>
+
+            <h3 className="text-sm font-Poppins font-bold text-black">
+              Helpline
+            </h3>
+            <p className="text-gray-800">1800 456 84788</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto px-4 mt-8 border-t pt-4">
+        <p className="text-center text-gray-600">
+          Arab Deals © 2023. All Rights Reserved
+        </p>
+      </div>
+    </footer>
+  );
 }
--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -5,12 +5,12 @@ import { FaUserAlt } from "react-icons/fa";
 import { FaShoppingBag } from "react-icons/fa";
 export default function Header() {
   return (
-    <div className="container mx-auto ms-5 lg:px-30 px-5 py-4 text-2xl font-serif flex justify-between  items-center  ">
+    <div className="container p-5 lg:px-16 text-2xl font-serif flex justify-between  items-center  ">
       
       <div><Link to="/">
         <img src={logo} alt="" />
       </Link></div>
-      <div className="flex justify-evenly w-1/4">
+      <div className="flex justify-end gap-10 w-1/4">
         <Link to="/">
           <FaUserAlt size={15}/>
         </Link>
